1b563bfb734b2fdff016076f3275f394ecdf523f
Ishidall <tishido.14670806@gmail.com>
2021-04-06 02:24:12 +0900

    Add tokenizer

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -4,11 +4,8 @@ function assert() {
   input="$2"
 
   ./9cc "$input" > tmp.s
-
   cc -o tmp tmp.s
-
   ./tmp
-
   actual="$?"
 
   if [ "$actual" = "$expected" ]; then
@@ -22,5 +19,6 @@ function assert() {
 assert 0 0
 assert 42 42
 assert 21 "5+20-4"
+assert 41 " 12 + 34 - 5 "
 
 echo OK
